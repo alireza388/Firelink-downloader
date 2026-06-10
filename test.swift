a5828ec5868261a6a71ec78f7030e7548ab5f4d0
NimBold <NimBold@users.noreply.github.com>
2026-06-10 12:28:07 +0330

    chore: remove temporary test file

--- test.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-print(Bundle.main.infoDictionary ?? "No Info.plist")
-let execPath = Bundle.main.executablePath
-print("Exec path: \(execPath ?? "nil")")
-if let path = execPath, let attr = try? FileManager.default.attributesOfItem(atPath: path), let modDate = attr[.modificationDate] as? Date {
-    print("Mod date: \(modDate)")
-}

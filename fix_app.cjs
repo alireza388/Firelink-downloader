18063dc4885f02c47ddd6bf5574fb4f74fc45100
NimBold <NimBold@users.noreply.github.com>
2026-06-17 11:07:01 +0330

    Remove unused and temporary files

--- fix_app.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-const idx1 = content.indexOf('import { invoke }');
-const idx2 = content.indexOf('export default function App() {', idx1);
-if (idx1 !== -1 && idx2 !== -1) {
-    // wait I injected it into App function body, wait where did I inject it?
-}
-// I will just git checkout src/App.tsx
--- revert_js.py
+++
@@ -1,7 +0,0 @@
-with open("src-tauri/src/lib.rs", "r") as f:
-    content = f.read()
-
-content = content.replace('.arg("--js-runtimes").arg(format!("deno:{},node", deno_path.display()));', '.arg("--js-runtimes").arg("deno,node");')
-
-with open("src-tauri/src/lib.rs", "w") as f:
-    f.write(content)
